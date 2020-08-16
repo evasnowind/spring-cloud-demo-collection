dc418c962a611654b8332d2a1e351acd527bdeb5
Chenglong Yu <yuchenglong8809@163.com>
2020-08-16 21:20:01 +0800

    init rocketmq demo

---
+++ spring-cloud-demo-rocketmq/src/test/java/com/prayerlaputa/rocketmq/RocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prayerlaputa.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
